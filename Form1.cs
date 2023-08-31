53eeec8fb72c9e059a8a0a1e1b61e8e3be24dc85
acee091 <138808454+acee091@users.noreply.github.com>
2023-08-31 17:09:29 -0300

    Calcula volume e area.cs

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Classes___Aula01
 {
     public partial class Form1 : Form
@@ -28,10 +30,50 @@ namespace Classes___Aula01
 
             //Montar resposta e exibir resultado no label
             string res = "O volume da caixa é: " + volume.ToString() + "cm³";
-            lblVolume.Text = res;
+            lblVolume.Text = res;           
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculaArea_Click(object sender, EventArgs e)
+        {
+            Retangulo rt = new Retangulo();
+            Triangulo tri = new Triangulo();
+
+            rt.baseRet = Convert.ToDouble(txtBaseRetangulo.Text);
+            rt.alturaRet = Convert.ToDouble(txtAltRetangulo.Text);
+
+            tri.alturaTrian = Convert.ToDouble(txtAltTriangulo.Text);
+            tri.baseTrian = Convert.ToDouble(txtBaseTriangulo.Text);
+
+            double AreaRet = rt.AreaRet();
+
+            double AreaTri = tri.AreaTrian();
+
+            string AreaRes = ("A área do retângulo é de " + AreaRet.ToString()+ "cm³\n" + 
+            "A área do triângulo é de " + AreaTri.ToString() + "cm³");
+            lblAreaRes.Text = AreaRes;
+        }
+
+        private void lblAreaRes_Click(object sender, EventArgs e)
         {
 
         }
@@ -46,6 +88,27 @@ namespace Classes___Aula01
         {
             double vol = altura * largura * profundidade;
             return vol;
+        }
+    }
+
+    class Retangulo
+    {
+        public double baseRet;
+        public double alturaRet;
+        public double AreaRet()
+        {
+            double volret = baseRet* alturaRet;
+            return volret;
+        }
+    }
+    class Triangulo
+    {
+        public double baseTrian;
+        public double alturaTrian;
+        public double AreaTrian()
+        {
+            double voltri = (baseTrian * alturaTrian) / 2;
+            return voltri;
         }
     }
 }
